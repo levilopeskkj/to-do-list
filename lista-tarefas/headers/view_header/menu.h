790196e3aa99878da11f7b868997d2bdb14bf733
Levi <levilopeskkj@gmail.com>
2025-06-04 17:31:34 -0300

    Add files via upload

---
+++ lista-tarefas/headers/view_header/menu.h
@@ -0,0 +1,9 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "../controler_header/tarefas.h"
+
+void mostrar_estatisticas(const Tarefa* tasks, int task_count);
+void mostrar_menu();
+
+#endif /* MENU_H */
---
+++ lista-tarefas/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//// biblioteca própria
+#include "headers/controler_header/tarefas.h"
+#include "headers/view_header/menu.h"
+
+//////
+
+#define MAX_TAREFAS 100
+#define MAX_DESCRIPTION 256
+
+Tarefa tasks[MAX_TAREFAS];
+int task_count = 0;
+int next_id = 1;
+
+int main() {
+    int escolha;
+    
+    printf(" Bem-vindo ao Todo List!\n");
+    
+    while (1) {
+        mostrar_menu();
+        if (scanf("%d", &escolha) != 1) {
+            printf(" Op o inv lida! Digite um n mero.\n");
+            continue;
+        }
+
+        switch (escolha) {
+            case 1:
+                int prioridade = 0;
+                adicionar_tarefa(tasks, &task_count, &next_id, &prioridade); 
+                break;
+            case 2:
+                listar_tarefas(tasks, task_count);
+                break;
+            case 3:
+                concluir_tarefa(tasks, &task_count);
+                break;
+            case 4:
+                remover_tarefa(tasks, &task_count);
+                break;
+            case 5:
+                mostrar_estatisticas(tasks, task_count);
+                break;
+            case 6:
+                alterar_tarefa(tasks, task_count); // Option 6 is Alterar tarefa
+                break;
+            case 7:
+                buscar_tarefa(tasks, task_count); // Option 7 is Buscar tarefa
+                break;
+            case 8:
+                printf(" Obrigado por usar o Todo List! At  logo!\n");
+                exit(0);
+            default:
+                printf(" Op o inv lida! Escolha entre 1 e 8.\n");
+        }
+        
+        printf("\nPressione Enter para Confirmar...");
+        getchar();
+        getchar();
+    }
+    
+    return 0;
+}
